f9342ae45229a09ac273ccbc9f23dfa7a8ffb0ab
cigsurya <cigsurya@gmail.com>
2026-02-23 22:53:32 -0600

    fix some styles

--- src/config/companyConfig.js
+++ src/config/companyConfig.js
@@ -1,6 +1,6 @@
 export const companyConfig = {
-  companyName: import.meta.env.VITE_COMPANY_NAME ?? "Nlinq Solutions",
+  companyName: import.meta.env.VITE_COMPANY_NAME ?? "Four LT Solutions",
   companyAddress: import.meta.env.VITE_COMPANY_ADDRESS ?? "440 Benmar Dr, Suite 3344, Houston, TX 77060",
   companyPhoneNumber: import.meta.env.VITE_COMPANY_PHONE ?? "307-228-1243",
-  companyEmail: import.meta.env.VITE_COMPANY_EMAIL ?? "hr@nlinq.com",
+  companyEmail: import.meta.env.VITE_COMPANY_EMAIL ?? "hr@fourlt.com",
 };
